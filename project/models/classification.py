83ec411dc5ed515d68f97d141fc6f0bd2d1f4777
Sami Barchid <sami.barchid@univ-lille.fr>
2021-11-22 12:34:55 +0100

    mnist classif with spikingjelly

---
+++ project/models/classification.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from spikingjelly.clock_driven import neuron, functional, surrogate, layer
+
+from project.models.utils import ConvBNLIF, LinearLIF
+
+
+class LeNet5(nn.Module):
+    """LeNet5 network for MNIST classification"""
+
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.conv1 = ConvBNLIF(1, 12, kernel_size=5)
+        self.conv2 = ConvBNLIF(12, 32, kernel_size=3, stride=2)  # 14
+        self.conv3 = ConvBNLIF(32, 64, kernel_size=3, stride=2)  # 7
+        self.flat = nn.Flatten(2)
+        self.fc1 = LinearLIF(64 * 7 * 7, 64)
+        self.fc_final = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.flat(x)
+        x = self.fc1(x)
+        out = self.fc_final(x.mean(0))
+        return out
---
+++ project/models/utils.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from spikingjelly.clock_driven import neuron, functional, surrogate, layer
+
+
+class ConvBNLIF(nn.Sequential):
+    """Some Information about ConvBNLIF"""
+
+    def __init__(self, in_channels, out_channels, kernel_size=3, dilation=1, stride=1):
+        super(ConvBNLIF, self).__init__()
+        padding = kernel_size // 2 + dilation - 1
+
+        self.add_module('conv_bn', layer.SeqToANNContainer(
+            nn.Conv2d(in_channels,
+                      out_channels,
+                      kernel_size=kernel_size,
+                      padding=padding,
+                      # no bias if we use BatchNorm (because it has a BN itself)
+                      bias=False,
+                      dilation=dilation,
+                      stride=stride),
+            nn.BatchNorm2d(out_channels)
+        ))
+
+        self.add_module('lif', neuron.MultiStepLIFNode(tau=2.0, v_threshold=1.0, v_reset=0.,
+                                                       surrogate_function=surrogate.ATan(alpha=2.0, spiking=True)))
+
+
+class ConvLIF(nn.Sequential):
+    """Some Information about ConvBNLIF"""
+
+    def __init__(self, in_channels, out_channels, kernel_size=3, dilation=1, stride=1):
+        super(ConvLIF, self).__init__()
+        padding = kernel_size // 2 + dilation - 1
+
+        self.add_module('conv_bn', layer.SeqToANNContainer(
+            nn.Conv2d(in_channels,
+                      out_channels,
+                      kernel_size=kernel_size,
+                      padding=padding,
+                      # no bias if we use BatchNorm (because it has a BN itself)
+                      bias=True,
+                      dilation=dilation,
+                      stride=stride)
+        ))
+
+        self.add_module('lif', neuron.MultiStepLIFNode(tau=2.0, v_threshold=1.0, v_reset=0.,
+                                                       surrogate_function=surrogate.ATan(alpha=2.0, spiking=True)))
+
+
+class LinearLIF(nn.Sequential):
+    def __init__(self, in_channels, out_channels):
+        self.add_module('fc', layer.SeqToANNContainer(
+            nn.Linear(in_channels, out_channels)
+        ))
+
+        self.add_module('lif', neuron.MultiStepLIFNode(tau=2.0, v_threshold=1.0, v_reset=0.,
+                                                       surrogate_function=surrogate.ATan(alpha=2.0, spiking=True)))
