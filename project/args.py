27aee87c49259363577747b54dd8bcfee972883a
Sami Barchid <sami.barchid@univ-lille.fr>
2021-11-23 19:11:55 +0100

    batch size rename

--- project/args.py
+++ project/args.py
@@ -14,7 +14,7 @@ def get_args():
                         help='number of total epochs to run')
     parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                         help='manual epoch number (useful on restarts)')
-    parser.add_argument('-b', '--batch-size', default=4, type=int,
+    parser.add_argument('-b', '--batch_size', default=4, type=int,
                         metavar='N', help='mini-batch size (default: 4).')
     parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float,
                         metavar='LR', help='initial learning rate (default: 1e-3).', dest='lr')
